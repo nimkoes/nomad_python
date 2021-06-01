a9ccd7abe08ec19f6bd96d9de045e7478ead8232
nimkoes <xxxelppa@gmail.com>
2021-06-01 23:35:05 +0900

    theory 1.0 data type of python

---
+++ throey/day_01/theory_1.0_data_type_of_python.py
@@ -0,0 +1,11 @@
+a_string = 'like this'
+a_number = 3
+a_float = 3.14
+a_boolean = False
+a_none = None
+
+print(type(a_string))
+print(type(a_number))
+print(type(a_float))
+print(type(a_boolean))
+print(type(a_none))
